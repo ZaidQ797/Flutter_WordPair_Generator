8d4930ee65ab653d509f7852a19ac5cf84fda4f7
Zaid Qureshi <53520463+ZaidQ797@users.noreply.github.com>
2020-11-05 01:55:18 +0500

    wordpair generator

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import './word_pair.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: ThemeData(primaryColor: Colors.purple[900]),
+        home: RandomWords());
+  }
+}
